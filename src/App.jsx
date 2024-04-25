47f2df4fbb98b193cc04938ac5f18f3b0619d432
Zaman <zamanofficial1980@gmail.com>
2024-04-25 20:11:16 +0530

    New repo of React context hook

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-slate-600 text-white p-5'>Zaman</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
