a98a1c8b0feae15a90fcfff3f2289b8b0744852e
Zaman <zamanofficial1980@gmail.com>
2024-04-25 22:31:23 +0530

    Adds some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import UserContextProvider from './Context/UserContextProvider'
 import Login from './Component/Login'
+import Profile from './Component/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div className="flex flex-col justify-center items-center gap-3 bg-slate-600  mx-60 my-8 p-5 rounded-2xl">
     <UserContextProvider>
-        <h1 className='bg-slate-600 text-white p-5'>Zaman</h1>
+        <h1 className='bg-slate-600 text-white p-5 text-center'>React Context Hook/APi</h1>
         <Login/>
+        <Profile/>
     </UserContextProvider>
+    </div>
   )
 }
 
--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,36 +1,38 @@
-import React, { useContext, useState } from 'react'
+import React, {useContext, useState} from 'react';
 import UserContext from '../Context/UserContext';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const{setUser} = useContext(UserContext);
+  const [username, setUsername] = useState ('');
+  const [password, setPassword] = useState ('');
+  const {setUser} = useContext (UserContext);
 
+  const handleSubmit = e => {
+    e.preventDefault ();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    setUser ({username, password});
+  };
 
-        setUser({username, password})
-
-    }
-    
   return (
-    <div className='flex flex-col justify-center items-center'>
-        <h1>Login</h1>
-        <input type='text'
+    <div className="flex flex-col justify-center items-center gap-3 text-white">
+      <h1>Login</h1>
+      <input
+        type="text"
         value={username}
-        onChange={(e)=>setUsername(e.target.value)}
-        placeholder='Username'
-         />
-         <input type='text'
-         value={password}
-         onChange={(e)=>setPassword(e.target.value)}
-         placeholder='Password'
-         />
+        onChange={e => setUsername (e.target.value)}
+        placeholder="Username"
+        className='text-black px-2 rounded-lg bg-white/25'
+      />
+      <input
+        type="text"
+        value={password}
+        onChange={e => setPassword (e.target.value)}
+        placeholder="Password"
+        className='text-black px-2 rounded-lg bg-white/25'
+      />
 
-        <button onClick={handleSubmit}>Submit</button>
+      <button className='bg-yellow-700 text-black px-3 py-1 rounded-xl' onClick={handleSubmit}>Submit</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
